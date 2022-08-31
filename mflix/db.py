6cf41c9e030c452176b36705d7a6b1c066685896
ghani <agaarbi@gmail.com>
2022-08-31 15:42:20 +0500

    ticket get comments

--- mflix/db.py
+++ mflix/db.py
@@ -272,6 +272,18 @@ def get_movie(id):
                 "$match": {
                     "_id": ObjectId(id)
                 }
+            },
+            {
+                "$lookup": {
+                    "from": 'comments',
+                    "let": { 'id': '$_id' },
+                    "pipeline": [
+                        { '$match':
+                            { '$expr': { '$eq': [ '$movie_id', '$$id' ] } }
+                        }
+                    ],
+                    "as": 'comments'
+                }
             }
         ]
 
@@ -522,8 +534,8 @@ def update_prefs(email, prefs):
         # TODO: User preferences
         # Use the data in "prefs" to update the user's preferences.
         response = db.users.update_one(
-            { "some_field": "some_value" },
-            { "$set": { "some_other_field": "some_other_value" } }
+            { "email": email },
+            { "$set": { "preferences": prefs } }
         )
         if response.matched_count == 0:
             return {'error': 'no user found'}
